2d3508c79b56293c74908b96a25d35390c0d3c71
Boaz Blake <BBlake@goempyrean.com>
2021-03-24 14:45:09 -0500

    biuls

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["app.css","8fab24e5bddd97a836ba06e5c307912f"],["app.js","41e241884f225ea7fa68553c3c8f7fab"],["index.html","66a08762aab5de2e830f9f29b74599d1"]];
+var precacheConfig = [["app.css","5acd53aaa42be0913082c86f30d3376e"],["app.js","516582b3cc11e875d1415fb41c404083"],["index.html","66a08762aab5de2e830f9f29b74599d1"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
